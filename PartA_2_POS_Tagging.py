35d2d638592a13af8bc1fd242c5538175663a488
TijnD <tijndijkkamp@hotmail.com>
2021-04-19 19:57:25 +0200

    New

---
+++ PartA_2_POS_Tagging.py
@@ -0,0 +1,40 @@
+from collections import Counter
+import spacy
+
+
+def input_to_tweets(file_name):
+    text_input = open(file, "r", encoding='utf8')
+    text_lines = text_input.readlines()
+    all_tweets = []
+    for line in text_lines[1:]:
+        all_tweets.append(line.split('\t')[2])  # only take data from 3th column (Tweets)
+    text_input.close()
+    all_tweets = [line[:-1] for line in all_tweets]
+    return all_tweets
+
+
+file = "SemEval2018-T3-train-taskB.txt"
+tweets = input_to_tweets(file)
+
+nlp = spacy.load('en_core_web_sm')
+token_count, len_word = 0, 0
+word_frequencies = Counter()
+
+for tweet in tweets:
+    doc = nlp(tweet)
+    for sentence in doc.sents:
+        words = []
+        for token in sentence:
+            words.append(token.text)
+            len_word += len(token)
+        word_frequencies.update(words)
+    token_count += len(doc)
+word_count = sum(word_frequencies.values())
+type_count = len(word_frequencies.keys())
+average_words_per_tweet = round(word_count / len(tweets), 2)
+average_word_length = round(len_word / token_count, 2)
+print("number of tokens: {}\n"
+      "number of words: {}\n"
+      "number of types: {}\n"
+      "average words per tweet: {}\n"
+      "average word length: {}".format(token_count, word_count, type_count, average_words_per_tweet, average_word_length))
